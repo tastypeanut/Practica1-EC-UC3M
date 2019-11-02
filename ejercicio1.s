aaec1c074ddc84d58b225b97e8896d7b63a58277
tastypeanut <tastypeanut@users.noreply.github.com>
2019-11-02 21:21:50 +0100

    Correjido el orden de restauración de pila.

--- ejercicio1.s
+++ ejercicio1.s
@@ -142,13 +142,13 @@
                 move $ra, $s4
 
                 #Restauramos $s
-                addi $sp , $sp , 24
                 lw $s0, ($sp)
                 lw $s1, 4($sp)
                 lw $s2, 8($sp)
                 lw $s3, 12($sp)
                 lw $s4, 16($sp)
                 lw $s5, 20($sp)
+                addi $sp , $sp , 24
         
                 #Realizamos las comparaciones $t0 = A $t1= B y dependiendo del resultado va a fin0 (devuelve 0)
                 #O a fin1 (devuleve uno) o pasa automaticamente a 2 que es empate
